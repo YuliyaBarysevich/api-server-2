4f6bd7695406552f0de0ff031ae7112c9598e7fe
Yuliya Barysevich <47508299+YuliyaBarysevich@users.noreply.github.com>
2021-07-27 17:04:33 -0400

    auth routes

---
+++ src/server.js
@@ -0,0 +1,42 @@
+'use strict'
+
+//3rd party dependencies 
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Esoteric Resources
+const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+
+const authRoutes = require('./auth/routes.js');
+// const v1Routes = require('./routes/v1.js');
+// const v2Routes = require('./routes/v2.js');
+
+// prepare the express app
+const app = express();
+
+//App Level middleware
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+// app.use('./api/v1', v1Routes);
+// app.use('./api/v2', v2Routes);
+app.use(authRoutes);
+
+//Catch errors
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = {
+  server: app, 
+  start: (port) => { 
+    app.listen(port, () => {
+      console.log(`Server is Up on ${port}`)
+    })
+  }
+}
