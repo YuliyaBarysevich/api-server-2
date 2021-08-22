56489e9fc08a6861ced7791a19fb786e2ebae1c8
Yuliya Barysevich <47508299+YuliyaBarysevich@users.noreply.github.com>
2021-08-22 17:45:06 -0400

    updated schema for store app

--- src/models/products/model.js
+++ src/models/products/model.js
@@ -4,10 +4,13 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
   name: { type: String, required: true },
-  category: { type: String, required: true, enum: ['Food', 'Electronics'] },
+  category: { type: String, required: true, enum: ['Makeup', 'Skin Care', 'Hair', 'Fragrance', 'Bath & Body'] },
   price: { type: Number, required: true },
   description: { type: String, required: true, default: 'Here is description' },
+  miniDescription: { type: String, required: true, default: 'Description goes here'},
   url: { type: String, default: 'https://tracerproducts.com/wp-content/uploads/2019/12/Product-Image-Coming-Soon.jpg' },
+  url2: {type: String, default: 'https://tracerproducts.com/wp-content/uploads/2019/12/Product-Image-Coming-Soon.jpg'},
+  url3: {type: String, default: 'https://tracerproducts.com/wp-content/uploads/2019/12/Product-Image-Coming-Soon.jpg'},
   total: { type: Number, default: 0 },
   inStock: { type: Number, default: 5 }
 });
